e2660994c6303492e2b665b7e010bc4c2abf5530
Yash Godara <yash17godara@gmail.com>
2023-12-10 21:00:46 +0530

    chore: eslint tailwindcss prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <h1>Next.js</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
     </>
-  )
+  );
 }
