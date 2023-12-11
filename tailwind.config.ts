b623bd93c09f130785a1eb5f7a73a27e96b05845
Yash Godara <yash17godara@gmail.com>
2023-12-11 16:13:41 +0530

    styles: tailwind.config updated | radix ui colors added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,64 @@
 import type { Config } from 'tailwindcss';
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './ui/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts}',
+    './content/**/*.mdx',
   ],
+  darkMode: ['class'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['var(--font-hubot)', ...defaultTheme.fontFamily.sans],
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      textColor: {
+        primary: 'var(--gray-12)',
+        secondary: 'var(--gray-11)',
+        tertiary: 'var(--gray-9)',
+        brand: 'var(--brand)',
+        link: 'var(--blue-10)',
+      },
+      backgroundColor: {
+        primary: 'var(--gray-1)',
+        secondary: 'var(--gray-4)',
+        secondaryA: 'var(--gray-a4)',
+        tertiary: 'var(--gray-3)',
+      },
+      borderColor: {
+        primary: 'var(--gray-6)',
+        secondary: 'var(--gray-4)',
+      },
+      ringOffsetColor: {
+        primary: 'var(--gray-12)',
+      },
+      keyframes: {
+        in: {
+          '0%': { transform: 'translateY(18px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        'in-reverse': {
+          '0%': { transform: 'translateY(-18px)', opacity: '0' },
+          '100%': { transform: 'translateY(0px)', opacity: '1' },
+        },
+      },
+      animation: {
+        in: 'in .6s both',
+        'in-reverse': 'in-reverse .6s both',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
 export default config;
