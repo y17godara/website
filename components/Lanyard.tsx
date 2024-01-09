96e58ba506cedfd35f73bc8b65b6eaedb6bcc2bb
Yash Godara <yash17godara@gmail.com>
2024-01-09 20:26:38 +0530

    styles: added VSCode Presence

--- components/Lanyard.tsx
+++ components/Lanyard.tsx
@@ -130,7 +130,7 @@ const Lanyard: FC = () => {
   if (!doing || (!vsCode && !doing.spotify)) return <section></section>;
   return (
     <>
-      <section className='relative flex'>
+      <section className='relative flex flex-col gap-2 md:gap-4 xl:gap-6'>
         <DisplayPresence doing={doing} />
       </section>
     </>
@@ -140,15 +140,15 @@ const Lanyard: FC = () => {
 const DisplayPresence = (Presence: any) => {
   const doing = Presence.doing;
   const currentDate: any = new Date();
+
   const currentActivity = useMemo(
-    () => doing?.activities.find((activity: any) => activity.type === 0),
+    () =>
+      doing?.activities.find(
+        ({ application_id }: any) => application_id === "383226320970055681"
+      ),
+
     [doing]
   );
-
-  useEffect(() => {
-    // console.log("currentActivity", { currentActivity });
-    // console.log("doing", { doing });
-  }, [Presence]);
   return (
     <>
       {doing?.listening_to_spotify ? (
@@ -197,30 +197,46 @@ const DisplayPresence = (Presence: any) => {
           </>
         </Link>
       ) : null}
-      {/* {currentActivity ? (
-          <Container href={"/presence"} {...props}>
-            <h5>{"Doing something"}</h5>
-            <ActivityRow>
-              {currentActivity.assets ? (
-                <ActivityImageContainer>
-                  <ActivityImage
-                    src={`https://cdn.discordapp.com/app-assets/${currentActivity.application_id}/${currentActivity.assets.large_image}.png`}
-                  />
-                  <ActivitySecondaryImage
-                    src={`https://cdn.discordapp.com/app-assets/${currentActivity.application_id}/${currentActivity.assets.small_image}.png`}
-                  />
-                </ActivityImageContainer>
+      {currentActivity ? (
+        <Link
+          className={cn(
+            "w-full cursor-pointer rounded-md border-[1px] border-secondary bg-transparent p-[1rem] text-secondary transition-colors duration-200 ease-in-out hover:bg-secondary hover:text-primary"
+          )}
+          href={"/presence"}
+        >
+          <h5 className='flex flex-row items-center justify-center gap-1 text-center'>
+            Doing something
+            <div className='inline-block h-2 w-2 animate-pulse rounded-full bg-[var(--brand)]'></div>
+          </h5>
+          <div className='flex flex-row items-center'>
+            {currentActivity.assets ? (
+              <div className={"relative h-[50px] md:h-[80px] lg:h-[100px]"}>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={`https://cdn.discordapp.com/app-assets/${currentActivity.application_id}/${currentActivity.assets.large_image}.png`}
+                  alt='Album Art'
+                  className='h-[50px] w-[50px] rounded-md md:h-[80px] md:w-[80px] lg:h-[100px] lg:w-[100px]'
+                />
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={`https://cdn.discordapp.com/app-assets/${currentActivity.application_id}/${currentActivity.assets.small_image}.png`}
+                  alt='Spotify Logo'
+                  className='absolute -bottom-[5px] -right-[5px] h-5 w-5 rounded-full border border-white bg-white dark:border-black dark:bg-black'
+                />
+              </div>
+            ) : null}
+            <div className='ml-4'>
+              <h5 className='m-0 text-primary'>{currentActivity.name}</h5>
+              {currentActivity.details ? (
+                <p className='m-0 text-sm font-thin'>
+                  {currentActivity.details}
+                </p>
               ) : null}
-              <ActivityInfo>
-                <h5>{currentActivity.name}</h5>
-                {currentActivity.details ? (
-                  <p>{currentActivity.details}</p>
-                ) : null}
-                {currentActivity.state ? <p>{currentActivity.state}</p> : null}
-              </ActivityInfo>
-            </ActivityRow>
-          </Container>
-        ) : null} */}
+              {currentActivity.state ? <p>{currentActivity.state}</p> : null}
+            </div>
+          </div>
+        </Link>
+      ) : null}
     </>
   );
 };
