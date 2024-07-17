5c8c4cc8b48fc77346f69df4bce589bc2491ac3e
Yash Godara <yash17godara@gmail.com>
2024-07-18 01:53:48 +0530

    styles: blogs ui

--- app/(mdx)/blogs/[slug]/page.tsx
+++ app/(mdx)/blogs/[slug]/page.tsx
@@ -55,6 +55,7 @@ export default async function page({ params }: { params: { slug: string } }) {
         className='flex animate-in flex-col gap-8'
         style={{ "--index": 1 } as React.CSSProperties}
       >
+        {/*
         {post.image && (
           <>
             <Image
@@ -69,34 +70,67 @@ export default async function page({ params }: { params: { slug: string } }) {
             />
           </>
         )}
+        */}
 
         <div
           className='flex max-w-3xl animate-in flex-col gap-4'
           style={{ "--index": 2 } as React.CSSProperties}
         >
-          <div className='flex w-full flex-col gap-1'>
+          <div className='flex w-full flex-col'>
             <h1 className='w-full text-3xl font-bold leading-tight tracking-tight text-primary'>
               {post.title}
             </h1>
-            <p className='text-lg leading-tight text-secondary md:text-xl'>
-              {post.description}
-            </p>
           </div>
 
-          <div className='flex max-w-none items-center gap-4'>
-            <Image
-              src={Avatar}
-              width={40}
-              height={40}
-              alt='avatar'
-              className='rounded-full bg-secondary'
-            />
-            <div className='leading-tight'>
-              <p className='font-medium text-primary'>Yash Godara</p>
+          <div className='flex flex-col gap-1.5'>
+            <div
+              className='flex flex-col gap-4 overflow-hidden rounded-lg border border-secondary'
+              style={{ "--index": 3 } as React.CSSProperties}
+            >
+              {post.image && (
+                <>
+                  <Image
+                    src={post.featuredImage}
+                    alt={`${post.title} post image`}
+                    width={700}
+                    height={350}
+                    className='-ml-6 w-[calc(100%+48px)] max-w-none animate-in md:rounded-lg lg:-ml-16 lg:w-[calc(100%+128px)]'
+                    priority
+                    quality={100}
+                  />
+                </>
+              )}
             </div>
-            <div className='leading-tight'>
-              <div className='text-secondary'>
-                <ViewCounter post={post as Post} />
+            <div className='flex w-full flex-col items-end gap-0.5 text-secondary'>
+              <h5 className='text-xs text-secondary'>
+                Last updated on {new Date(post.updatedAt).toDateString()}
+              </h5>
+            </div>
+          </div>
+
+          <p className='text-base leading-tight text-secondary md:text-lg'>
+            {post.description}
+          </p>
+
+          <div
+            className='flex flex-col items-start justify-start gap-4 md:flex-row md:items-center md:justify-between'
+            style={{ "--index": 4 } as React.CSSProperties}
+          >
+            <div className='flex max-w-none items-center gap-4'>
+              <Image
+                src={Avatar}
+                width={40}
+                height={40}
+                alt='avatar'
+                className='rounded-full bg-secondary'
+              />
+              <div className='leading-tight'>
+                <p className='font-medium text-primary'>Yash Godara</p>
+              </div>
+              <div className='leading-tight'>
+                <div className='text-secondary'>
+                  <ViewCounter post={post as Post} />
+                </div>
               </div>
             </div>
           </div>
@@ -105,7 +139,7 @@ export default async function page({ params }: { params: { slug: string } }) {
 
       <article
         className='blog prose-white prose animate-in dark:prose-invert lg:prose-lg'
-        style={{ "--index": 3 } as React.CSSProperties}
+        style={{ "--index": 5 } as React.CSSProperties}
       >
         <MdxWrapper code={post.body.code} />
       </article>
