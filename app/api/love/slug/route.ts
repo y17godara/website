bfdb1321f36d0def8e0350f16c2a356f7f9f570d
Yash Godara <yash17godara@gmail.com>
2024-07-18 16:29:03 +0530

    fix #1

--- app/api/love/slug/route.ts
+++ app/api/love/slug/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
-export const dynamic = "auto";
+export const dynamic = "force-dynamic";
 // 'auto' | 'force-dynamic' | 'error' | 'force-static'
 
 export async function GET(req: NextRequest) {
